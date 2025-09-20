a0d81d277df9d0ce307828852bdb070245969406
pilarcsa <pilarcsa.99@gmail.com>
2025-09-20 17:06:36 +0200

    Remove unnecessary files and update project structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+process.env.NODE_ENV === "production"? console.log("modo produccion"):console.log("modo desarrollo");
 
 const app = express();
 //express.json nos sirve para procesar datos con json
@@ -16,7 +16,7 @@ app.get("/post/:id", (req,res) => {
     res.send(`el id recibido es:${req.params.id}`);
 })
 
-app.listen(process.env.PORT||3000,() => {
+app.listen(process.env.PORT,() => {
     console.log("servidor corriendo en puerto localhost 3000.");
     console.log(process.env.PORT);
 });
