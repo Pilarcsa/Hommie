482657d5b32ede2b1394055d6cacb289d3f4c1c2
pilarcsa <pilarcsa.99@gmail.com>
2025-10-17 17:47:47 +0200

    Adjust code for render

--- src/auth/auth-controller.js
+++ src/auth/auth-controller.js
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'lax',
             maxAge: 3600 * 1000,
         });
@@ -43,41 +43,4 @@ export const login = async (req, res) => {
         return response.sendError(res, `Error en el servidor: ${error.message}`, 500);
     }
 };
-
-
-
-
-
-/*import response from "../utils/response.js"
-import serviceUser from "../components/users/user-service.js" 
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-
-export const login = async (req,res) => {
-
-    const {email , password} = req.body
-    
-    try{
-       const user = await serviceUser.getUserByEmail(email) 
-     
-    if (!user) return  response.sendSucess(res, "email o contraseña incorrecta",null,404)
-        const passwordIsValid = await bcrypt.compare(password, user.password)
-    if (!passwordIsValid) return  response.sendSucess(res, "email o contraseña incorrecta h",null,404)
-       
-        const token =  jwt.sign({user}, process.env.JWT_SECRET, {expiresIn: "1h"})
-     
-        delete user.password
-   
-        res.cookies("token", token, {
-            httpOnly : true,
-            secure : false,
-            sameSite : "lax",
-            maxAge : 3600*1000,
-        })
-  
-        return response.sendSucess(res, "login exitoso", {user})
-    }catch(error){
-        response.sendError(res, "error del servidor");
-    }
-}*/
 
