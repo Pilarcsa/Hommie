d5a00ffdaed7f1b52138a3927d9e340bf073205d
pilarcsa <pilarcsa.99@gmail.com>
2026-04-25 21:05:41 +0200

    user model modification

--- src/components/posts/post-model.js
+++ src/components/posts/post-model.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const postSchema = new mongoose.Schema({
   userId: {
   type: mongoose.Schema.Types.ObjectId,
-  ref: "User",
+  ref: "user",
   required: true,
   index: true,
   },
