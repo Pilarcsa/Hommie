8453e938db3d72f0c67d08b32ff2841a69aed123
Pilar Carrascosa Sanz-Agero <pilarcsa.99@gmail.com>
2026-09-12 13:11:42 +0200

    Configure dotenv for production and development
    Load environment variables from different files based on NODE_ENV.

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ import postRouter from "./src/components/posts/post-router.js"
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import cors from "cors";
-dotenv.config();
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.prod' });
+} else {
+  dotenv.config({ path: '.env.develop' });
+}
 
 // Indica si la aplicación se ejecuta en modo producción o desarrollo
 process.env.NODE_ENV === "production" ? console.log("modo produccion") : console.log("modo desarrollo");
