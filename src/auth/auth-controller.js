b5a80405b273fc28e4a376bbf022202b2d4aff6c
pilarcsa <pilarcsa.99@gmail.com>
2026-09-14 21:43:59 +0200

    button

--- src/auth/auth-controller.js
+++ src/auth/auth-controller.js
@@ -29,6 +29,7 @@ export const login = async (req, res) => {
 
         // Crea un token JWT y lo guarda como cookie segura
         delete user.password;
+        console.log(process.env.JWT_SECRET);
           if (!process.env.JWT_SECRET) {
           throw new Error('JWT_SECRET no está definida')
         }
